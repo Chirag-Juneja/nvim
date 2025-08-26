fdba0493b614afcdeaa2f1daf7951dd0a03e1d0f
Chirag Juneja <chiragjuneja6@gmail.com>
2025-08-27 01:55:42 +0530

    autocomplete

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -22,23 +22,13 @@ return {
         "neovim/nvim-lspconfig",
         lazy = false,
         config = function()
-            -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
             local lspconfig = require("lspconfig")
-            -- lspconfig.tsserver.setup({
-            --   capabilities = capabilities
-            -- })
-            -- lspconfig.solargraph.setup({
-            --   capabilities = capabilities
-            -- })
-            -- lspconfig.html.setup({
-            --   capabilities = capabilities
-            -- })
-            -- lspconfig.lua_ls.setup({
-            --   capabilities = capabilities
-            -- })
-            lspconfig.lua_ls.setup({})
-            lspconfig.gopls.setup({})
+            lspconfig.lua_ls.setup({ capabilities = capabilities })
+            lspconfig.pylsp.setup({ capabilities = capabilities })
+            lspconfig.rust_analyzer.setup({ capabilities = capabilities })
+            lspconfig.gopls.setup({ capabilities = capabilities })
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
