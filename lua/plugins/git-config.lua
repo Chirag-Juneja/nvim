ddd0bda8f8fb2174e2658ca6635693ab1a95cbe5
Chirag Juneja <chiragjuneja6@gmail.com>
2025-08-28 22:49:31 +0530

    git fugitive, gitsigns & catpuccin

---
+++ lua/plugins/git-config.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup()
+            vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk_inline<CR>", {})
+        end,
+    },
+    {
+        "tpope/vim-fugitive",
+        config = function ()
+            vim.keymap.set("n","<leader>gs",":Git<CR>",{})
+        end
+    },
+}
